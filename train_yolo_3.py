e3cc49f095181bf69fab1f1c3ab1da585da845ea
JDMR12 <jdmrubia@gmail.com>
2025-05-14 02:02:23 +0800

    Update train_yolo_3.py

--- train_yolo_3.py
+++ train_yolo_3.py
@@ -3,7 +3,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLOv8n model
-model = YOLO('runs/detect/train11/weights/best.pt')
+model = YOLO('runs/detect/train13/weights/best.pt')
 
 # Run inference 
 model.predict('test_images', save=True, imgsz=640, conf=0.2)
